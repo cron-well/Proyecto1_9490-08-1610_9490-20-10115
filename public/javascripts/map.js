94a469d52c3a8d83733380031a708738ba059040
Angel Godoy <agodoy.lh@gmail.com>
2023-11-02 15:22:08 -0600

    ADD/2da fase

---
+++ public/javascripts/map.js
@@ -0,0 +1,13 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibS0xMzVhIiwiYSI6ImNrOGsyb3ZqaDBkemkzcW10emc1eXoyNngifQ.NuSNrMKqrpdm-jxvPpx0_Q";
+const lat = 14.63333300;
+const lng = -90.60000000;
+
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  zoom: 8,
+  center: [lng, lat],
+});
+
+const marker = new mapboxgl.Marker().setLngLat([lng, lat]).addTo(map);
